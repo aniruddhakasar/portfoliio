805648131a7ba08e7f2c2b0e5410f4ab80f44681
aniruddhakasar <aniruddhakasar2001@gmail.com>
2023-11-23 12:16:06 +0530

    update

--- forms/contact.php
+++ forms/contact.php
@@ -7,9 +7,9 @@
   */
 
   // Replace contact@example.com with your real receiving email address
-  $receiving_email_address = 'contact@example.com';
+  $receiving_email_address = 'aniruddhakasar2001@gmail.com';
 
-  if( file_exists($php_email_form = '../assets/vendor/php-email-form/php-email-form.php' )) {
+  if( file_exists($php_email_form = 'D:/potfolio/iPortfolio/assets/vendor/php-email-form/php-email-form.php' )) {
     include( $php_email_form );
   } else {
     die( 'Unable to load the "PHP Email Form" Library!');
